37b9bbc0e69b7f3ef04c4a01961d78acbb59f2c9
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2025-05-16 14:27:52 +0400

    Added decrement-increment

---
+++ decrement-increment.js
@@ -0,0 +1,35 @@
+console.log("decrement-increment.js");
+
+//Binary Operators
+console.log(2 + 2); // 4
+console.log(10 - 1); // 9
+console.log(5 * 10); // 50
+console.log(100 / 25); // 4
+
+console.log(2 ** 10); // 1024
+console.log(13 % 5); // 3
+console.log(5 % 2); // 1
+
+// Unary Operators
+console.log(-10); // -10
+
+console.log(+true); // 1
+console.log(+false); // 0
+console.log(+"100"); // 100
+console.log(+" "); // 0
+console.log(+""); // 0
+console.log(+null); // 0
+console.log(+undefined); // NaN
+console.log(+{}); // NaN
+console.log(+[]); // 0
+
+let count = 10;
+count += 2
+
+// Decrement
+count--;
+console.log(count); // 9
+
+// Increment
+count++;
+console.log(count); // 10
